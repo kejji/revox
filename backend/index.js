92309f1e2c1cd392fa4bc9dbee98c7132b6da712
Slimane Kejji <slimane.kejji@gmail.com>
2025-07-07 22:01:22 +0200

    use library vendia to handle both API GW and Fuction URL

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
+
 const AWS_REGION        = process.env.AWS_REGION;
 const QUEUE_URL         = process.env.EXTRACTION_QUEUE_URL;
 const EXTRACTIONS_TABLE = process.env.EXTRACTIONS_TABLE;
+
 const express = require("express");
-const cors = require("cors");
+const cors    = require("cors");
 const { checkJwt } = require("./auth");
+
+// Import de la logique d’extraction
+const { createExtraction } = require("./routes/extract");
 
 const app = express();
 app.use(cors());
@@ -13,17 +18,24 @@ app.use(express.json());
 // Route publique
 app.get("/health", (_, res) => res.send({ status: "OK" }));
 
-// Route protégée
+// Route protégée dashboard
 app.get("/dashboard", checkJwt, (req, res) => {
   res.json({ message: "Données sensibles pour " + req.auth.sub });
 });
 
-// Ce bloc est utilisé en local uniquement
+// Route protégée extraction
+app.post("/extract", checkJwt, createExtraction);
+
+// Pour le dev local uniquement
 if (process.env.LOCAL === "true") {
   app.listen(4000, () => console.log("API on http://localhost:4000"));
 }
 
 // Export du handler Lambda
-const awsServerlessExpress = require("aws-serverless-express");
-const server = awsServerlessExpress.createServer(app);
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+const serverlessExpress = require("@vendia/serverless-express");
+const server = serverlessExpress({ app });
+
+exports.handler = (event, context) => {
+  // @vendia/serverless-express gère automatiquement API GW v1, v2 et Function URLs
+  return server(event, context);
+};
