4e0825c69ae034a4e75c7d524e734934d358c07d
Slimane Kejji <slimane.kejji@gmail.com>
2025-07-14 17:16:23 +0200

    rename cognito env variables and fix sh script to generate them

---
+++ infra/generate-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+REPO="kejji/revox"
+AWS_REGION="eu-west-3"
+COGNITO_USER_POOL_ID=$(terraform output -raw cognito_user_pool_id)
+COGNITO_APP_CLIENT_ID=$(terraform output -raw cognito_app_client_id)
+API_URL=$(terraform output -raw http_api_endpoint)
+
+cat <<EOF > ../frontend/.env
+AWS_REGION=$AWS_REGION
+COGNITO_USER_POOL_ID=$COGNITO_USER_POOL_ID
+COGNITO_APP_CLIENT_ID=$COGNITO_APP_CLIENT_ID
+API_URL=$API_URL
+EOF
+
+echo "✓ .env frontend generated in ../frontend/.env"
+
+cat <<EOF > ../backend/.env
+AWS_REGION=$AWS_REGION
+COGNITO_USER_POOL_ID=$COGNITO_USER_POOL_ID
+COGNITO_APP_CLIENT_ID=$COGNITO_APP_CLIENT_ID
+SQS_QUEUE_URL=$(terraform output -raw extraction_queue_url)
+DYNAMODB_TABLE_NAME=$(terraform output -raw extractions_table_name)
+EOF
+
+echo "✓ .env backend generated in ../backend/.env"
+
+gh secret set COGNITO_USER_POOL_ID -b"$COGNITO_USER_POOL_ID" --repo "$REPO"
+gh secret set COGNITO_APP_CLIENT_ID -b"$COGNITO_APP_CLIENT_ID" --repo "$REPO"
+gh secret set API_URL -b"$API_URL" --repo "$REPO"
