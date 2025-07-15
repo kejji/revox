c860908843976895955dd89e9f861a588c554851
Slimane Kejji <slimane.kejji@gmail.com>
2025-07-15 22:08:43 +0200

    trimspace for all terraform outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,31 +1,31 @@
 output "cognito_user_pool_id" {
-  value = aws_cognito_user_pool.revox_user_pool.id
+  value = trimspace(aws_cognito_user_pool.revox_user_pool.id)
 }
 
 output "cognito_app_client_id" {
-  value = aws_cognito_user_pool_client.revox_app_client.id
+  value = trimspace(aws_cognito_user_pool_client.revox_app_client.id)
 }
 
 output "extractions_table_name" {
-  value = aws_dynamodb_table.extractions.name
+  value = trimspace(aws_dynamodb_table.extractions.name)
 }
 
 output "extraction_queue_url" {
   description = "URL de la queue SQS pour les extractions"
-  value = aws_sqs_queue.extraction_queue.url
+  value = trimspace(aws_sqs_queue.extraction_queue.url)
 }
 
 output "extraction_queue_arn" {
   description = "ARN de la queue SQS pour les extractions"
-  value       = aws_sqs_queue.extraction_queue.arn
+  value       = trimspace(aws_sqs_queue.extraction_queue.arn)
 }
 
 output "http_api_endpoint" {
   description = "URL publique de l’HTTP API (stage $default)"
-  value       = aws_apigatewayv2_api.http_api.api_endpoint
+  value       = trimspace(aws_apigatewayv2_api.http_api.api_endpoint)
 }
 
 output "function_url" {
   description = "URL directe de la Lambda pour debug"
-  value       = aws_lambda_function_url.api_url.function_url
+  value       = trimspace(aws_lambda_function_url.api_url.function_url)
 }
